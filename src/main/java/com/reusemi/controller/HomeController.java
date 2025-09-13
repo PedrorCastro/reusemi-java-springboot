de7fa00734509b402a28c5b8af5d3d873f20bb44
PedrorCastro <pedrorcastro@hotmail.com>
2025-09-13 13:28:15 -0300

    commit-7

--- src/main/java/com/reusemi/controller/HomeController.java
+++ src/main/java/com/reusemi/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.reusemi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
     @GetMapping("/")
